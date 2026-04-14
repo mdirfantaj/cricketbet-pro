a1dc6087791654e96599be23216f55baecdd29cb
MD IRFAN <231236152+mdirfantaj@users.noreply.github.com>
2026-04-14 21:19:28 +0530

    Update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("✅ MongoDB Connected");
+
+        if (!process.env.MONGO_URL) {
+            throw new Error("MONGO_URL not found in .env");
+        }
+
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("✅ MongoDB Connected Successfully");
+
     } catch (err) {
         console.log("❌ MongoDB Error:", err.message);
         process.exit(1);
